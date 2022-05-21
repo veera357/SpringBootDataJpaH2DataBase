10a9359b497fa7e7a2da71b6ebaa6a3dfb07bcd7
manjula <makelamn12@gmail.com>
2022-05-21 18:46:47 +0100

    manju commit

--- SpringBootDataJPAWithH2/src/main/java/in/veera/dataJpa/DBOperationRunner.java
+++ SpringBootDataJPAWithH2/src/main/java/in/veera/dataJpa/DBOperationRunner.java
@@ -2,6 +2,7 @@ package in.veera.dataJpa;
 
 import java.util.Arrays;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
